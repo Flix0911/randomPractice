a8b0f35583b9dbb85d402dda558698906045ed87
Erick Valencia <evalencia0911@gmail.com>
2023-11-26 18:09:09 -0700

    more array goodtimes

--- random.js
+++ random.js
@@ -416,40 +416,127 @@
 //Mix it up
 
 //1. Given the following array const myArray = [5 ,10 ,500, 20]
-const myArray = [5 ,10 ,500, 20]
+//const myArray = [5 ,10 ,500, 20]
 
 //2.Using the push method, add the string "Egon" to the end of the array.
 
-myArray.push("Egon")
-console.log(myArray)
+// myArray.push("Egon")
+// console.log(myArray)
 
 //3. Using a method, remove the string from the end of the array.
 
-myArray.pop()
-console.log(myArray)
+// myArray.pop()
+// console.log(myArray)
 
 //4. Using the unshift method, add the string "Bob Marley" to the beginning of the array
 
-myArray.unshift("Bob Marley")
-console.log(myArray)
+// myArray.unshift("Bob Marley")
+// console.log(myArray)
 
 //5. Using a different method, remove the string from the beginning of the array
 
-myArray.splice(0, 1)
-console.log(myArray)
+// myArray.splice(0, 1)
+// console.log(myArray)
 
 //6. Use the reverse method on this array
 
-myArray.reverse()
-console.log(myArray)
+// myArray.reverse()
+// console.log(myArray)
 
 //Biggie smalls
 //Write an if..else statement that iterates over the myArray array declared above:
 
-for (let i = 0; i < myArray.length; i++) {
-    if (myArray[i] < 21) {
-        console.log(`It seems ${myArray[i]} is less than 21`);
-    }else {
-        console.log(`It seems ${myArray[i]} is greater than 21`);
-    }
-} 
+// for (let i = 0; i < myArray.length; i++) {
+//     if (myArray[i] < 21) {
+//         console.log(`It seems ${myArray[i]} is less than 21`);
+//     }else {
+//         console.log(`It seems ${myArray[i]} is greater than 21`);
+//     }
+// } 
+
+//Monkey in the middle
+
+// const myNumber = 'banana';
+
+// if(myNumber < 5) {
+//     console.log("little number")
+// } else if (myNumber > 10) {
+//     console.log("big number")
+// } else {
+//     console.log("monkey")
+// }
+
+
+
+// ----
+//What's in your closet
+
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+//1. What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+
+console.log("Kristyn is rocking that " + kristynsCloset[3] + " today!")
+
+//2. Kristyn can't find her left shoe. Remove this item from her closet and save it to a variable named kristynShoe.
+
+kristynShoe = kristynsCloset.splice(0, 1)
+console.log(kristynShoe);
+console.log(kristynsCloset);
+
+//3. Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
+
+kristynsCloset.splice(5, 0, "raybans")
+console.log(kristynsCloset)
+
+//4. Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
+kristynsCloset[4] = "stained knit hat"
+console.log(kristynsCloset)
+
+
+//5. Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
+
+console.log("Looks like Thom is wearing " + thomsCloset[0][2] + " ," + thomsCloset[1][2] + ", and" + thomsCloset[2][2] + " today!")
+
+//6. In the same way, access one item from Thom's pants array.
+console.log(thomsCloset[1][1])
+
+//7. Access one item from Thom's accessories array.
+console.log(thomsCloset[2][0])
+
+//8. Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+console.log("Thom is looking fierce in a " + thomsCloset[0][0] + ", " + thomsCloset[1][1] + " and " + thomsCloset[2][1] + "!")
+
+//9. Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+
+thomsCloset[1][2] = "Footie Pajamas"
+
+console.log(thomsCloset[1])
