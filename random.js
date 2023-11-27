770d87cefc76bface798b0a1a4492400f48b175e
Erick Valencia <evalencia0911@gmail.com>
2023-11-26 18:23:21 -0700

    moving to the next lab

--- random.js
+++ random.js
@@ -471,72 +471,101 @@
 // ----
 //What's in your closet
 
-const kristynsCloset = [
-    "left shoe",
-    "cowboy boots",
-    "right sock",
-    "GA hoodie",
-    "green pants",
-    "yellow knit hat",
-    "marshmallow peeps"
-  ];
+// const kristynsCloset = [
+//     "left shoe",
+//     "cowboy boots",
+//     "right sock",
+//     "GA hoodie",
+//     "green pants",
+//     "yellow knit hat",
+//     "marshmallow peeps"
+//   ];
   
-  // Thom's closet is more complicated. Check out this nested data structure!!
-  const thomsCloset = [
-    [
-      // These are Thom's shirts
-      "grey button-up",
-      "dark grey button-up",
-      "light blue button-up",
-      "blue button-up",
-    ],[
-      // These are Thom's pants
-      "grey jeans",
-      "jeans",
-      "PJs"
-    ],[
-      // Thom's accessories
-      "wool mittens",
-      "wool scarf",
-      "raybans"
-    ]
-  ];
+//   // Thom's closet is more complicated. Check out this nested data structure!!
+//   const thomsCloset = [
+//     [
+//       // These are Thom's shirts
+//       "grey button-up",
+//       "dark grey button-up",
+//       "light blue button-up",
+//       "blue button-up",
+//     ],[
+//       // These are Thom's pants
+//       "grey jeans",
+//       "jeans",
+//       "PJs"
+//     ],[
+//       // Thom's accessories
+//       "wool mittens",
+//       "wool scarf",
+//       "raybans"
+//     ]
+//   ];
 
 //1. What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
 
-console.log("Kristyn is rocking that " + kristynsCloset[3] + " today!")
+// console.log("Kristyn is rocking that " + kristynsCloset[3] + " today!")
 
 //2. Kristyn can't find her left shoe. Remove this item from her closet and save it to a variable named kristynShoe.
 
-kristynShoe = kristynsCloset.splice(0, 1)
-console.log(kristynShoe);
-console.log(kristynsCloset);
+// kristynShoe = kristynsCloset.splice(0, 1)
+// console.log(kristynShoe);
+// console.log(kristynsCloset);
 
 //3. Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
 
-kristynsCloset.splice(5, 0, "raybans")
-console.log(kristynsCloset)
+// kristynsCloset.splice(5, 0, "raybans")
+// console.log(kristynsCloset)
 
 //4. Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
-kristynsCloset[4] = "stained knit hat"
-console.log(kristynsCloset)
+// kristynsCloset[4] = "stained knit hat"
+// console.log(kristynsCloset)
 
 
 //5. Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
 
-console.log("Looks like Thom is wearing " + thomsCloset[0][2] + " ," + thomsCloset[1][2] + ", and" + thomsCloset[2][2] + " today!")
+// console.log("Looks like Thom is wearing " + thomsCloset[0][2] + " ," + thomsCloset[1][2] + ", and" + thomsCloset[2][2] + " today!")
 
 //6. In the same way, access one item from Thom's pants array.
-console.log(thomsCloset[1][1])
+// console.log(thomsCloset[1][1])
 
 //7. Access one item from Thom's accessories array.
-console.log(thomsCloset[2][0])
+// console.log(thomsCloset[2][0])
 
 //8. Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
-console.log("Thom is looking fierce in a " + thomsCloset[0][0] + ", " + thomsCloset[1][1] + " and " + thomsCloset[2][1] + "!")
+// console.log("Thom is looking fierce in a " + thomsCloset[0][0] + ", " + thomsCloset[1][1] + " and " + thomsCloset[2][1] + "!")
 
 //9. Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
 
-thomsCloset[1][2] = "Footie Pajamas"
+// thomsCloset[1][2] = "Footie Pajamas"
 
-console.log(thomsCloset[1])
+// console.log(thomsCloset[1])
+
+
+// ------
+//HFM
+
+//Write a loop that prints the numbers in order
+// const twoDArray = [
+//     [9,8,7],
+//     [6,5,4],
+//     [3,2,1],
+//   ];
+
+// let arrayValue = ""
+
+// for (let arrayValue of twoDArray.sort()) {
+//     console.log(arrayValue)
+// }
+
+// for (let i = 0; i < twoDArray.length; i++) {
+//     for (let j = 0; j < twoDArray[i].length; j++) {
+//       console.log(twoDArray[i][j]);
+//     }
+//   }
+
+//   for (const row of twoDArray) {
+//     for (const element of row) {
+//       console.log(element);
+//     }
+//   }
